d8b9bf135453852c5186b2ef4819d5f3fd8aeb1b
kaktuxmx <kaktuxsql@gmail.com>
2017-12-15 00:37:59 -0700

    I cant make the Firebase to save the results independently, right now is overwriting my entries.

--- script.js
+++ script.js
@@ -10,3 +10,40 @@
    messagingSenderId: "577200030529"
  };
  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = "";
+
+//Capture click form the HTML
+$("#submit").on("click", function() {
+//Prevent page to refresh in a sec
+event.preventDefault();
+
+trainName = $("#trainName").val().trim();
+destination = $("#destination").val().trim();
+firstTrainTime = $("#firstTrainTime").val().trim();
+frequency = $("#destination").val().trim();
+
+console.log(trainName);
+console.log(destination);
+console.log(firstTrainTime);
+console.log(frequency);
+
+database.ref().set({
+trainName: trainName,
+destination: destination,
+firstTrainTime: firstTrainTime,
+frequency: frequency
+
+});
+
+});
+
+//database.ref().on("value", function(snapshot){
+
+
+})
