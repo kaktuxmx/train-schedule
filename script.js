e03e8c8486b8916d1fffced4d1b321d6a74b55de
kaktuxmx <kaktuxsql@gmail.com>
2017-12-14 23:35:14 -0700

    Added Firebase to JS and HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyA22HVLbx5RUAy0Hf5OXKPBuBj-ut0WEN8",
+   authDomain: "kaktux-train-project.firebaseapp.com",
+   databaseURL: "https://kaktux-train-project.firebaseio.com",
+   projectId: "kaktux-train-project",
+   storageBucket: "kaktux-train-project.appspot.com",
+   messagingSenderId: "577200030529"
+ };
+ firebase.initializeApp(config);
